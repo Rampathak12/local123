210311a76353bc750a08053c31ffdd69ec753d37
Rampathak12 <pathakram095@gmail.com>
2026-06-01 11:12:22 +0900

    Fix Render database configuration

--- render-start.sh
+++ render-start.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -eu
 
-if [ -n "${DATABASE_URL:-}" ]; then
-  url_without_scheme="${DATABASE_URL#postgresql://}"
-  credentials="${url_without_scheme%@*}"
-  host_and_database="${url_without_scheme#*@}"
-  username="${credentials%%:*}"
-  password="${credentials#*:}"
-  host_and_port="${host_and_database%%/*}"
-  database="${host_and_database#*/}"
-  database="${database%%\?*}"
-
-  export SPRING_DATASOURCE_URL="jdbc:postgresql://${host_and_port}/${database}"
-  export SPRING_DATASOURCE_USERNAME="${username}"
-  export SPRING_DATASOURCE_PASSWORD="${password}"
+if [ -z "${DATABASE_URL:-}" ]; then
+  echo "DATABASE_URL is not set. Add Render's Internal Database URL to the DATABASE_URL environment variable." >&2
+  exit 1
 fi
 
-exec java -jar /app/app.jar --spring.profiles.active=prod
+url_without_scheme="${DATABASE_URL#postgresql://}"
+credentials="${url_without_scheme%@*}"
+host_and_database="${url_without_scheme#*@}"
+username="${credentials%%:*}"
+password="${credentials#*:}"
+host_and_port="${host_and_database%%/*}"
+database="${host_and_database#*/}"
+database="${database%%\?*}"
+
+exec java -jar /app/app.jar \
+  --spring.profiles.active=prod \
+  --spring.datasource.url="jdbc:postgresql://${host_and_port}/${database}" \
+  --spring.datasource.username="${username}" \
+  --spring.datasource.password="${password}"
